1251bbc9f8a1e3ce57480e03a8eca492edf74abf
Igor Duarte <igor.duartematos@gmail.com>
2022-01-25 17:47:25 -0300

    feat: Create Page Header component - Created component style and structure - Added geolocation - Added route navigation

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,171 @@
+<template>
+  <header class="custom-header">
+    <h1 @click="redirectToProductList" class="title">MagaNets</h1>
+    <div class="navigation-box">
+      <ul class="list-wrapper">
+        <li>
+          <LocationDotIcon />
+          Cidade: {{ userCity }}
+        </li>
+        <li @click="callPhoneCentral" class="button">
+          <PhoneIcon />
+          Central de atendimento
+        </li>
+        <li @click="redirectToWishList" class="button">
+          <HeartIcon />
+          Lista de desejos
+        </li>
+      </ul>
+      <input type="text" class="input" placeholder="Busca" />
+    </div>
+  </header>
+</template>
+
+<script>
+import HeartIcon from "@/components/icons/HeartIcon";
+import LocationDotIcon from "@/components/icons/LocationDotIcon";
+import PhoneIcon from "@/components/icons/PhoneIcon";
+import axios from "axios";
+export default {
+  name: "PageHeader",
+
+  components: {
+    HeartIcon,
+    LocationDotIcon,
+    PhoneIcon,
+  },
+
+  data() {
+    return {
+      userLatitude: undefined,
+      userLongitude: undefined,
+      userCity: "...",
+    };
+  },
+
+  created() {
+    this.getUserLocation();
+  },
+
+  methods: {
+    callPhoneCentral() {
+      window.open("tel:3125574039");
+    },
+
+    async getStreetAddressFrom() {
+      try {
+        var { data } = await axios.get(
+          "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+            this.Userlatitude +
+            "," +
+            this.Userlongitude +
+            "&key=AIzaSyAMdrjXpdrf58MVgdRYw9MUngqpzk8Ds-Q"
+        );
+        console.log("data", data);
+        if (data.error_message) {
+          console.log(data.error_message);
+          console.log("if", data);
+        } else {
+          console.log("else", data);
+          this.userCity =
+            data.results[0].address_components[3].long_name +
+            " - " +
+            data.results[0].address_components[4].short_name;
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+
+    getUserLocation() {
+      const success = (position) => {
+        this.Userlatitude = position.coords.latitude;
+        this.Userlongitude = position.coords.longitude;
+
+        this.getStreetAddressFrom();
+      };
+
+      const error = (err) => {
+        console.log(err);
+        this.userCity = "Localização desabilitada";
+      };
+
+      navigator.geolocation.getCurrentPosition(success, error);
+    },
+
+    redirectToProductList() {
+      this.$router.push({ name: "ProductList" });
+    },
+
+    redirectToWishList() {
+      this.$router.push({ name: "WishList" });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.custom-header {
+  background-color: $brand-primary;
+  color: $white;
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+  gap: 5rem;
+
+  > .title {
+    font-size: 1.5rem;
+    font-weight: bold;
+    cursor: pointer;
+  }
+}
+
+.navigation-box {
+  width: 100%;
+  text-align: left;
+
+  svg {
+    width: 10px;
+  }
+
+  > .input {
+    width: 100%;
+    padding: 0.7rem;
+    border: 0;
+    border-radius: 5px 5px;
+    background-image: url("../assets/images/search-icon.svg");
+    background-size: 15px;
+    background-position: 10px 11px;
+    background-repeat: no-repeat;
+    padding-left: 35px;
+  }
+}
+
+.list-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+
+  > .button {
+    cursor: pointer;
+  }
+
+  > .button:hover {
+    text-decoration: underline;
+  }
+}
+
+@media (max-width: 768px) {
+  .custom-header {
+    flex-direction: column;
+  }
+
+  .list-wrapper {
+    flex-direction: column;
+    text-align: center;
+    gap: 5px;
+  }
+}
+</style>
